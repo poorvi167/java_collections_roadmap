dba51c6ff1e3b29b308b9167f26a71bb953f257f
dbc2201 <divyanshb22@gmail.com>
2022-03-29 12:59:13 +0530

    rename HelloController to MainController

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/MainController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/MainController.java
@@ -3,7 +3,7 @@ package io.github.dbc.java_collections_roadmap.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
