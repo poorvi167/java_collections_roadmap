c6defa296bcf3fb5eb953e8459cd5bfbb6c45ab7
dbc2201 <divyanshb22@gmail.com>
2022-03-30 02:53:10 +0530

    create IDs for the buttons

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/UniqueElementsView.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/UniqueElementsView.java
@@ -1,4 +1,13 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class UniqueElementsView {
+
+    @FXML
+    public Button uniqueYesButton;
+
+    @FXML
+    public Button uniqueNoButton;
 }
