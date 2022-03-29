4f3d490cbcaf5f5edc7052457b69c105bf547d78
dbc2201 <divyanshb22@gmail.com>
2022-03-30 03:10:56 +0530

    open the key-value pairs view

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/UniqueElementsView.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/UniqueElementsView.java
@@ -1,9 +1,15 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
+import io.github.dbc.java_collections_roadmap.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UniqueElementsView {
 
@@ -30,7 +36,19 @@ public class UniqueElementsView {
     }
 
     @FXML
-    public void onUniqueNoButtonClicked() {
+    public void onUniqueNoButtonClicked() throws IOException {
         // open a new stage
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("key-value-elements-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                Screen.getPrimary().getBounds().getMinX(),
+                Screen.getPrimary().getBounds().getMinY()
+        );
+        stage.setTitle("Key-Value Pairs");
+        stage.setScene(scene);
+        stage.show();
+        // close the current stage
+        closeCurrentStage();
     }
 }
