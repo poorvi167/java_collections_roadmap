59c984966e8392462b1879fde6c18092fe9ffaae
dbc2201 <divyanshb22@gmail.com>
2022-03-30 02:23:47 +0530

    open new stage to ask whether the number of elements is constant

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/MainController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/MainController.java
@@ -1,8 +1,15 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
+import io.github.dbc.java_collections_roadmap.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
 
@@ -13,7 +20,17 @@ public class MainController {
     private Label welcomeText;
 
     @FXML
-    protected void onMainButtonClick() {
+    protected void onMainButtonClick() throws IOException {
         // open the next stage here!
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("constant-elements-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                Screen.getPrimary().getBounds().getMinX(),
+                Screen.getPrimary().getBounds().getMinY()
+        );
+        stage.setTitle("Number of Elements");
+        stage.setScene(scene);
+        stage.show();
     }
 }
