8c86f7967c4131dba20fe259a55c02a801fa5c80
dbc2201 <divyanshb22@gmail.com>
2022-03-30 03:02:21 +0530

    add alert when yes button is clicked, and add a method to close the current stage

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/UniqueElementsView.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/UniqueElementsView.java
@@ -1,7 +1,9 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class UniqueElementsView {
 
@@ -14,6 +16,17 @@ public class UniqueElementsView {
     @FXML
     public void onUniqueYesButtonClicked() {
         // ask the user to user java.util.Set
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Unique Elements");
+        alert.setHeaderText("Use java.util.Set!");
+        alert.setContentText("Since you want to store a unique elements only, you should use java.util.Set.");
+        alert.setResizable(false);
+        alert.showAndWait();
+    }
+
+    private void closeCurrentStage() {
+        Stage stage = (Stage) uniqueYesButton.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
