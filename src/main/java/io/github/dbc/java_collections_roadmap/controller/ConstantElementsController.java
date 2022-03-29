f062a38e113aad37524ade250d00d0ed574faa97
dbc2201 <divyanshb22@gmail.com>
2022-03-30 02:28:50 +0530

    create buttons and methods

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/ConstantElementsController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/ConstantElementsController.java
@@ -1,4 +1,22 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ConstantElementsController {
+    @FXML
+    public Button constantsYesButton;
+
+    @FXML
+    public Button constantsNoButton;
+
+    private void onConstantsYesButtonClicked() {
+        // open the next scene
+    }
+
+    private void onConstantsNoButtonClicked() {
+        // declare the result
+        // close the current scene
+
+    }
 }
