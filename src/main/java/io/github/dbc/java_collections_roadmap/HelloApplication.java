837c5e61b85473a7903c13e0c494736075967ea5
dbc2201 <divyanshb22@gmail.com>
2022-03-29 12:57:03 +0530

    increase the screen size

--- src/main/java/io/github/dbc/java_collections_roadmap/HelloApplication.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/HelloApplication.java
@@ -3,6 +3,7 @@ package io.github.dbc.java_collections_roadmap;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +16,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                Screen.getPrimary().getBounds().getMinX(),
+                Screen.getPrimary().getBounds().getMinY()
+        );
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
