087f3bf2c444397d19dd83e21a72e74616bf6fe7
dbc2201 <divyanshb22@gmail.com>
2022-03-30 03:17:32 +0530

    attach methods to buttons

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/KeyValueElementsController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/KeyValueElementsController.java
@@ -1,6 +1,7 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 public class KeyValueElementsController {
@@ -13,11 +14,21 @@ public class KeyValueElementsController {
 
     @FXML
     public void onKeyValueYesButtonClicked() {
-
+        // ask the user to use java.util.Map
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Key-Value Elements");
+        alert.setHeaderText("use java.util.Map");
+        alert.setContentText("Use java.util.Map to store key-value pairs.");
+        alert.showAndWait();
     }
 
     @FXML
     public void onKeyValueNoButtonClicked() {
-
+        // ask the user to use java.util.List
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Key-Value Elements");
+        alert.setHeaderText("use java.util.List");
+        alert.setContentText("Use java.util.List if you don't want to store key-value pairs, and allow duplicates.");
+        alert.showAndWait();
     }
 }
