386f215b56fa5ac9ba62d285fde74fbc27061338
dbc2201 <divyanshb22@gmail.com>
2022-03-30 02:45:13 +0530

    close the constant number of elements stage, and open the unique elements stage

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/ConstantElementsController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/ConstantElementsController.java
@@ -1,8 +1,12 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
+import io.github.dbc.java_collections_roadmap.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,6 +34,21 @@ public class ConstantElementsController {
     private void onConstantsNoButtonClicked() throws IOException {
         // open the next stage
         Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("unique-elements-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                Screen.getPrimary().getBounds().getMinX(),
+                Screen.getPrimary().getBounds().getMinY()
+        );
+        stage.setTitle("Number of Elements");
+        stage.setScene(scene);
+        stage.show();
         // close the current stage
+        closeCurrentStage();
+    }
+
+    private void closeCurrentStage() {
+        Stage stage = (Stage) constantsYesButton.getScene().getWindow();
+        stage.close();
     }
 }
