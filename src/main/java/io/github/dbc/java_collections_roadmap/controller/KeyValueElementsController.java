efb133ae468df8eed97fb4564162a7e82dc21f4f
dbc2201 <divyanshb22@gmail.com>
2022-03-30 03:08:55 +0530

    add layout views for the key-value pairs view; create IDs for buttons

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/KeyValueElementsController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/KeyValueElementsController.java
@@ -1,4 +1,14 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class KeyValueElementsController {
+
+    @FXML
+    public Button keyValueYesButton;
+
+    @FXML
+    public Button keyValueNoButton;
+
 }
