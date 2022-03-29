553f179e2a4413a9920f85a0504c69f9bc8094a4
dbc2201 <divyanshb22@gmail.com>
2022-03-29 12:57:43 +0530

    rename hello-view.fxml to main-view.fxml

--- src/main/java/io/github/dbc/java_collections_roadmap/HelloApplication.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(
                 fxmlLoader.load(),
                 Screen.getPrimary().getBounds().getMinX(),
