5a0c9acc5310428e09cd2d1c089b3f4d0b00ecc4
poorvi167 <poorvi.gla_cs.ccv19@gla.ac.in>
2022-05-18 15:26:55 +0530

    defined the functionality of method sortingYesButtonPressed()

--- src/main/java/io/github/dbc/java_collections_roadmap/controller/SortElementsController.java
+++ src/main/java/io/github/dbc/java_collections_roadmap/controller/SortElementsController.java
@@ -1,6 +1,7 @@
 package io.github.dbc.java_collections_roadmap.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 public class SortElementsController {
@@ -13,7 +14,11 @@ public class SortElementsController {
 
     @FXML
     public void sortingYesButtonPressed() {
-
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Sort");
+        alert.setHeaderText("Methods for sorting Sets");
+        alert.setContentText("Use List and the method java.util.Collections.sort() for sorting, \nOr use TreeSet to sort the elements,\nOr use ObjectReference.stream().sorted() to sort the elements ");
+        alert.showAndWait();
     }
 
     @FXML
